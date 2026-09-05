36a62e1564ea8f9589ad0d5a5674588083158632
Jualerex <juandabaronariza@gmail.com>
2026-09-04 21:29:06 -0500

    Juan_Baron usuarios, mecanicos, proveedores

--- ConexionFastPoo/src/main/java/org/example/Main.java
+++ ConexionFastPoo/src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ public class Main {
         usuarios[0] = new Mecanico("Carlos Pérez", "carlos@taller.com", "Taller El Pits");
         usuarios[1] = new Proveedor("Distribuidora MotoRepuestos", "ventas@motorepuestos.com", "Local Central #4");
 
-        System.out.println("=== DEMOSTRACIÓN DE POLIMORFISMO DE USUARIOS ===\n");
+        System.out.println("=== DEMOSTRACIÓN DE POLIMORFISMO DE USUARIOS  ===\n");
 
         // Ejecución dinámica del método mostrarMenu() según el tipo de usuario
         for (Usuario u : usuarios) {
