718f4af81956c528e7ddd3398b35cd74f8614776
Chaskilon34 <jp.lozanoherrera@gmail.com>
2026-09-05 00:20:26 -0500

    Subida de mi parte a main - Juan Lozano

--- ConexionFastPoo/src/main/java/org/example/Main.java
+++ ConexionFastPoo/src/main/java/org/example/Main.java
@@ -8,12 +8,30 @@ public class Main {
         usuarios[0] = new Mecanico("Carlos Pérez", "carlos@taller.com", "Taller El Pits");
         usuarios[1] = new Proveedor("Distribuidora MotoRepuestos", "ventas@motorepuestos.com", "Local Central #4");
 
-        System.out.println("=== DEMOSTRACIÓN DE POLIMORFISMO DE USUARIOS  ===\n");
+        System.out.println("=== DEMOSTRACIÓN DE POLIMORFISMO DE USUARIOS ===\n");
 
-        // Ejecución dinámica del método mostrarMenu() según el tipo de usuario
         for (Usuario u : usuarios) {
             u.mostrarMenu();
             System.out.println();
+        }
+
+        /// Parte de Inventario / Repuestos --- temporal para valida el correcto funcionamiento y que mis compañero lo pueden editar
+        System.out.println("=== Funcionamiento de repuestos e inventario  ===\n");
+
+        /// crear obgetos inventario
+        Inventario inventario = new Inventario();
+        inventario.agregarRepuesto(new Repuestos("Guaya de freno", 15000, 10));
+        inventario.agregarRepuesto(new Repuestos("Bujía", 8000, 20));
+
+       ///llamarlos metodos para revisar la lista
+        inventario.listarRepuestos();
+
+        System.out.println("\nBuscando 'Bujía'...");
+        Repuestos encontrado = inventario.buscarPorNombre("Bujía");
+        if (encontrado != null) {
+            System.out.println("Encontrado: " + encontrado); ///encontro el objeto buscado antes con el metodo   inventario.buscarPorNombre
+        } else {
+            System.out.println("No se encontró el repuesto.");  ///lo que ejecuta el null cunado no encuentra nada
         }
     }
 }
---
+++ ConexionFastPoo/src/main/java/org/example/Repuestos.java
@@ -0,0 +1,52 @@
+package org.example;
+
+public class Repuestos {
+    private String nombre;
+    private int precio;
+    private int stock;
+
+    public Repuestos(String nombre, int precio, int stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    /// Metodos GET
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public int getPrecio() {
+        return this.precio;
+    }
+
+    public int getStock() {
+        return this.stock;
+    }
+
+    /// Metodos Set
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    ///  validar que el precio no tenga ningun numero negativo
+    public void setPrecio(int precio) {
+        if (precio < 0) {
+            System.out.println("Error: el precio no puede ser negativo.");
+        } else {
+            this.precio = precio;
+        }
+    }
+    /// validar que el Stock no tenga ningun numero negativo
+    public void setStock(int stock) {
+        if (stock < 0) {
+            System.out.println("Error: el stock no puede ser negativo.");
+        } else {
+            this.stock = stock;
+        }
+    }
+ /// toString es un metodo especial para  leer las 3 variables ya declaradas y las convina en un texto legible para la consola
+    @Override
+    public String toString() {
+        return "Repuesto: " + nombre + "  Precio: $" + precio + "  Stock: " + stock;
+    }
+}
